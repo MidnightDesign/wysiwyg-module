5f469f7c9603bdc44461a9d8cbbe7773f85bb456
Rudolph Gottesheim <r.gottesheim@loot.at>
2014-05-08 16:10:47 +0200

    Update CKEditor and simplify view helper API

--- lib/Midnight/Wysiwyg/View/Helper/Wysiwyg.php
+++ lib/Midnight/Wysiwyg/View/Helper/Wysiwyg.php
@@ -3,30 +3,25 @@
 namespace Midnight\Wysiwyg\View\Helper;
 
 use Zend\View\Helper\AbstractHelper;
-use Zend\View\Renderer\PhpRenderer;
 
 class Wysiwyg extends AbstractHelper
 {
-    static $defaultOptions = [
-        'images_url' => null,
-    ];
-
-    function __invoke($text, $save_url, array $options = [])
+    function __invoke($text)
     {
-        if (isset($options['condition'])) {
-            if (!$options['condition'] === true) {
-                return $text;
-            }
-        }
-        $options = $options + self::$defaultOptions;
-        if (!$text) {
-            $text = '<p></p>';
-        }
-        /** @var $renderer PhpRenderer */
-        $renderer = $this->getView()->getEngine();
-        $renderer->headScript()->appendFile($renderer->basePath('midnight/wysiwyg/vendor/ckeditor/ckeditor.js'));
-        $renderer->headScript()->appendFile($renderer->basePath('midnight/wysiwyg/js/wysiwyg.js'));
-        $renderer->headLink()->appendStylesheet($renderer->basePath('midnight/wysiwyg/css/wysiwyg.css'));
-        return '<div class="wysiwyg" contenteditable="true" data-save-url="' . $save_url . '" data-images-url="' . $options['images_url'] . '">' . $text . '</div>';
+        $view = $this->getView();
+        $basePath = $view->plugin('basePath');
+
+        $headScript = $view->plugin('headScript');
+        $headScript->appendFile($basePath('midnight/wysiwyg/vendor/ckeditor/ckeditor.js'));
+        $headScript->appendFile($basePath('midnight/wysiwyg/js/wysiwyg.js'));
+
+        $headLink = $view->plugin('headLink');
+        $headLink->appendStylesheet($basePath('midnight/wysiwyg/css/wysiwyg.css'));
+
+        return '<div'
+        . ' contenteditable="true"'
+        . '>'
+        . $text
+        . '</div>';
     }
 }
